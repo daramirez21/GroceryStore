0281b1a271992f55832cb15be3068b5c004569d7
daramirez21 <daramirez@mail.csuchico.edu>
2019-10-07 17:24:55 -0700

    Final

--- main.cpp
+++ main.cpp
@@ -1,32 +1,46 @@
+//Author: David Ramirez
 #include<iostream>
 #include<string>
 using namespace std;
 
 int main()
 {
-  string list[5]; //array of 5 strings
+  string list[5];  //array of 5 strings
   int numItems = 0;
   char input;
-  
+  string item;
 
-  do {
-
-
-
+  do
+  {
 
   cout<<"\n==GROCERY LIST MANAGER==";
   cout<<"\nEnter your choice: ";
   cout<<"\n (A)dd an item";
-
-  
   cout<<"\n (Q)uit";
   cout<<"\nYour choice (A/Q): ";
   cin>>input;
   
- 
+  if (input == 'a' || input == 'A')
+  {
+    cout<< "What is the item?\n";
+    cin>>item;
+    
+
+  if(numItems > 5)
+  {
+   list[numItems] = item;
+   numItems++;
+  }
+
+   else
+     { 
+     cout<< "You'll need a bigger list!\n";
+     } 
+
+  }
+
 
   } while (input != 'q' && input != 'Q');
-
 
   return 0;
 }
